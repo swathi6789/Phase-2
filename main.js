0a70a39c594937a8f014e8a44d4df1261846d6c8
swathi6789 <40848654+swathi6789@users.noreply.github.com>
2019-02-13 16:26:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+var request;
+var open;
+var tx;
+var store;
+var result;
+var indexedDB= window.indexedDB|| window.webkitIndexedDB || window.msIndexedDB || window.mozIndexedDB;
+request=indexedDB.open("mydatabase",1);
+request.onerror=function(e){
+  console.log("error"+e);
+}
+request.onupgradeneeded=function(e){
+result=e.target.result;
+store=result.createObjectStore("resume",{keyPath:"name"});
+}
+request.onsuccess=function(e){
+  result= e.target.result;
+    function getdata(callback){
+      tx=result.transaction("resume",IDBTransaction.READ_ONLY);
+      store=tx.objectStore("resume");
+      data=[];
+      tx.oncomplete=function(e)  {
+        callback(result);
+      console.log(result);
+  }
+      var cursor=store.openCursor();
+      cursor.onerror=function(e)
+      {
+        console.log("error"+e)
+      }
+      cursor.onsuccess=function(e)
+      {
+        var dn=e.target.result;
+        if(dn)
+        {
+        data.push(dn.value);
+        dn.continue();
+      }
+      }
+    }
+    var parent=document.querySelector(".parent");
+    getdata(function(d){
+      console.log(d);
+     for(var i in data){
+       var child=document.createElement("div");
+       child.classList.add("child");
+       parent.appendChild(child);
+
+       var img=document.createElement("img");
+       img.src="download.jpeg";
+       img.alt=data[i].name;
+       child.appendChild(img);
+
+       var name=document.createElement("h2");
+       name.textContent=data[i].name;
+        child.appendChild(name);
+
+        var email=document.createElement("h2");
+        email.textContent=data[i].email;
+         child.appendChild(email);
+
+         var button=document.createElement("a");
+         button.textContent="view profile";
+          button.href="resume.html?name="+data[i].name;
+        child.appendChild(button);
+     }
+    });
+  }
